a7226683028a35bd31996e97d06b4e6e5345182c
Anuj Paul <anujpaul19@outlook.com>
2026-03-08 13:57:11 -0500

    Added Properties Get and set Metadata option

--- BlobController.cs
+++ BlobController.cs
@@ -1,14 +1,16 @@
+using System.ComponentModel;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api")]
 public class BlobController
 {
-    
+
     [HttpGet("test")]
     public string Test()
     {
@@ -18,6 +20,7 @@ public class BlobController
     [HttpPost("readblob")]
     public async Task<string> ReadBlob([FromBody] BlobRequest req)
     {
+
         var serviceClient = new BlobServiceClient(
             new Uri($"https://{req.StorageAccount}.blob.core.windows.net"),
             new DefaultAzureCredential()
@@ -34,6 +37,40 @@ public class BlobController
         var content = await stream.ReadToEndAsync();
 
         return content;
+    }
+
+    [HttpPost("getContainerProperties")]
+    public async Task<BlobContainerProperties> getContainerProperties([FromBody] BlobRequest req)
+    {
+        IDictionary<string, string> metadata = new Dictionary<string, string>();
+
+        var serviceClient = new BlobServiceClient(
+            new Uri($"https://{req.StorageAccount}.blob.core.windows.net"),
+            new DefaultAzureCredential()
+        );
+
+        var containerClient = serviceClient.GetBlobContainerClient(req.ContainerName);
+
+        var properties = await containerClient.GetPropertiesAsync();
+
+        return properties;
+    }
+
+    [HttpPost("setcontainermetadata")]
+    public async Task<BlobContainerProperties> setContainerMetadata([FromBody] BlobRequest req)
+    {
+        IDictionary<string, string> metadata = new Dictionary<string, string>();
+
+        metadata.Add("Belongs_to", "Anuj");
+
+        var blobService = new BlobServiceClient(new Uri($"https://{req.StorageAccount}.blob.core.windows.net"),
+                            new DefaultAzureCredential());
+
+        var container = blobService.GetBlobContainerClient(req.ContainerName);
+
+        await container.SetMetadataAsync(metadata);
+        var properties = await container.GetPropertiesAsync();
+        return properties;
     }
 
 }
